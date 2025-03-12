f0d750e1adba3b0cf7a45c98df54925d00a9d2a4
Raza Hassan <bilgramiraza@yahoo.com>
2025-03-12 13:37:09 +0000

    Added Context Store to the Project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BillsContextProvider } from './reducer/billsReducer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BillsContextProvider>
+      <App />
+    </BillsContextProvider>
   </StrictMode>,
-)
+);
