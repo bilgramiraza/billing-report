13c26893a52a9776084ebf4a584b5d1096506755
Raza Hassan <bilgramiraza@yahoo.com>
2025-03-12 13:41:01 +0000

    Displaying Store content

--- src/components/ReportPanel.jsx
+++ src/components/ReportPanel.jsx
@@ -1,8 +1,27 @@
+import { useBillsValue } from "../reducer/billsReducer";
 
 const ReportPanel = () => {
+  const { formOne, formTwo, formOneStatus, formTwoStatus } = useBillsValue();
+
+  if (!(formOneStatus && formTwoStatus)) {
+    return (
+      <div>
+        <p>Report Panel</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <p>Report Panel</p>
+      <p>Form One: </p>
+      <p>Total: {formOne.total}</p>
+      <p>Days: {formOne.days}</p>
+      <p>Units: {formOne.units}</p>
+      <p>Form Two: </p>
+      <p>Total: {formTwo.total}</p>
+      <p>Days: {formTwo.days}</p>
+      <p>Units: {formTwo.units}</p>
     </div>
   );
 };
