0dc0c70c5e72a26c600eaea4f5e56ae5a56e26c1
Raza Hassan <bilgramiraza@yahoo.com>
2025-03-12 11:12:12 +0000

    Setup Context for Forms

---
+++ src/reducer/billsReducer.jsx
@@ -0,0 +1,83 @@
+import { createContext, useContext, useReducer } from "react";
+
+const initialState = {
+  formOne: {},
+  formTwo: {},
+  submitToggle: false,
+};
+
+const billsReducer = (state, action) => {
+  switch (action.type) {
+    case 'setFormOne':
+      return {
+        ...state,
+        formOne: action.payload,
+      };
+    case 'setFormTwo':
+      return {
+        ...state,
+        formTwo: action.payload,
+      };
+    case 'toggleSubmit':
+      return {
+        ...state,
+        submitToggle: !state.submitToggle,
+      };
+    case 'clear':
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const BillsContext = createContext();
+
+export const BillsContextProvider = (props) => {
+  const [bills, dispatch] = useReducer(billsReducer, initialState);
+
+  return (
+    <BillsContext.Provider value={[bills, dispatch]}>
+      {props.children}
+    </BillsContext.Provider>
+  );
+};
+
+export const useBillsValue = () => {
+  const billsAndDispatch = useContext(BillsContext);
+  return billsAndDispatch[0];
+};
+
+export const useBillsDispatch = () => {
+  const billsAndDispatch = useContext(BillsContext);
+  return billsAndDispatch[1];
+};
+
+export const setFormOne = (dispatch, total, days, units) => {
+  dispatch({
+    type: 'setFormOne',
+    payload: {
+      total,
+      days,
+      units,
+    },
+  });
+};
+
+export const setFormTwo = (dispatch, total, days, units) => {
+  dispatch({
+    type: 'setFormTwo',
+    payload: {
+      total,
+      days,
+      units,
+    },
+  });
+};
+
+export const clearForms = (dispatch) => {
+  dispatch({
+    type: 'clear',
+  });
+};
+
+export default BillsContext;
