7724779965d4c93801e6cc7f817dab50309137de
Raza Hassan <bilgramiraza@yahoo.com>
2025-03-12 09:37:29 +0000

    Setup Forms for Two bills

--- src/components/FormOne.jsx
+++ src/components/FormOne.jsx
@@ -1,8 +1,30 @@
+import { useInputs } from "../services/hooks";
 
 const FormOne = () => {
+  const total = useInputs('total', 'number');
+  const days = useInputs('days', 'number');
+  const units = useInputs('units', 'number');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Form 1: \nTotal: ${total.value} \nDays: ${days.value}\nUnits: ${units.value}`);
+  };
+
   return (
     <div>
       <p>Form One</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Total: <input {...total} />
+        </label>
+        <label>
+          Days: <input {...days} />
+        </label>
+        <label>
+          Units: <input {...units} />
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
--- src/components/FormTwo.jsx
+++ src/components/FormTwo.jsx
@@ -1,8 +1,30 @@
+import { useInputs } from "../services/hooks";
 
 const FormTwo = () => {
+  const total = useInputs('total', 'number');
+  const days = useInputs('days', 'number');
+  const units = useInputs('units', 'number');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Form 2: \nTotal: ${total.value} \nDays: ${days.value}\nUnits: ${units.value}`);
+  };
+
   return (
     <div>
       <p>Form Two</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Total: <input {...total} />
+        </label>
+        <label>
+          Days: <input {...days} />
+        </label>
+        <label>
+          Units: <input {...units} />
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
